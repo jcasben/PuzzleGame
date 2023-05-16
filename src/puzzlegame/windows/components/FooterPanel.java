7e502f8860ac7e4fa68a76eabb1c52aa42fb377c
Jesus Castillo <jcasben02@gmail.com>
2023-05-16 19:51:11 +0200

    [UPDATE] Made some changes

--- src/puzzlegame/windows/components/FooterPanel.java
+++ src/puzzlegame/windows/components/FooterPanel.java
@@ -7,28 +7,10 @@ public class FooterPanel extends JPanel {
     public FooterPanel() {
         setVisible(true);
         setLayout(new GridLayout(1,1));
-        //initProgressBar();
-        add(new JButton("CONTINUAR"));
-    }
-
-    private void initProgressBar() {
-        JProgressBar progressBar = new JProgressBar();
-        progressBar.setSize(new Dimension(400,30));
-        progressBar.setValue(0);
-        int i = 0;
-        while(i<100) {
-            progressBar.setValue(i);
-            i++;
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        add(progressBar);
     }
 
     private void footerButton() {
-
+        JButton bcontinue = new JButton("CONTINUAR");
+        //bcontinue.addActionListener(e -> new MainPanel());
     }
 }
