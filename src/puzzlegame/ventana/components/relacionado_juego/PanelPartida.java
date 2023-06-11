8abb1d8d15c2adcc246342f53bd3a4e0b84261ee
jcasben <jcasben02@gmail.com>
2023-06-11 22:51:58 +0200

    [UPDATE] Changed the time ratio.

--- src/puzzlegame/ventana/components/relacionado_juego/PanelPartida.java
+++ src/puzzlegame/ventana/components/relacionado_juego/PanelPartida.java
@@ -21,7 +21,7 @@ public class PanelPartida extends JPanel {
         JSplitPane jsp2 = new JSplitPane(
                 JSplitPane.VERTICAL_SPLIT,
                 new PanelSubImagen(img.getDivisiones(), img.getFilas(), img.getColumnas()),
-                PanelTemporizador.iniPanTemp(img.getFilas() * img.getColumnas() * 2)
+                PanelTemporizador.iniPanTemp(img.getFilas() * img.getColumnas() * 3)
         );
         jsp2.setEnabled(false);
         jsp2.setDividerLocation(675);
